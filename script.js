8f7f1c0915d1060cc01268e31ec606f4e5093fc2
NishantTheProgrammer <knishant113@gmail.com>
2020-06-29 22:45:04 +0530

    Img path issue

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let len = 4, i = 1;
 
 setInterval(()=>{
     if(i > len) i = 1;
-    screen.backgroundImage = `url('img/ad${i++}.jpg')`
+    screen.backgroundImage = `url('./img/ad${i++}.jpg')`
 }, 2000);       //This function will run every 2s
