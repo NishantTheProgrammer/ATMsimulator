fb8cb458881a867a945ba9e38772d498099c5e8f
NishantTheProgrammer <knishant113@gmail.com>
2020-06-29 21:24:24 +0530

    Template design ready for logic

---
+++ README.md
@@ -0,0 +1,2 @@
+Live on https://nishanttheprogrammer.github.io/ATMsimulator/
+Video live coding video https://youtu.be/025lPzicWus
---
+++ script.js
@@ -0,0 +1,8 @@
+const screen = document.querySelector('.screen').style;
+
+let len = 4, i = 1;
+
+setInterval(()=>{
+    if(i > len) i = 1;
+    screen.backgroundImage = `url('../img/ad${i++}.jpg')`
+}, 2000);       //This function will run every 2s
